facf63fc99d4f2ad5296a42cff6e04ef9362b6da
Scott.Moon <interactord@gmail.com>
2022-02-24 14:10:16 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LinkNavigator
+
+A description of this package.
---
+++ Tests/LinkNavigatorTests/LinkNavigatorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LinkNavigator
+
+final class LinkNavigatorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LinkNavigator().text, "Hello, World!")
+    }
+}
