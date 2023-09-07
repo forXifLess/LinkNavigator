f314240aaa3a6cbc18759a8b20cf5f76b046030c
Scott Moon <47267960+interactord@users.noreply.github.com>
2023-09-07 23:05:42 +0900

    fix: Remove unnecessary code and print statements.

--- Sources/LinkNavigator/Components/LinkNavigationView.swift
+++ Sources/LinkNavigator/Components/LinkNavigationView.swift
@@ -8,7 +8,6 @@ public struct LinkNavigationView<ItemValue: EmptyValueType> {
   let item: LinkItem<ItemValue>?
 
   public init(linkNavigator: SingleLinkNavigator<ItemValue>, item: LinkItem<ItemValue>? = .none) {
-    print("AAA inint")
     self.linkNavigator = linkNavigator
     self.item = item
   }
@@ -17,19 +16,14 @@ public struct LinkNavigationView<ItemValue: EmptyValueType> {
 // MARK: UIViewControllerRepresentable
 
 extension LinkNavigationView: UIViewControllerRepresentable {
-  public static func dismantleUIViewController(_: UINavigationController, coordinator _: ()) {
-    print("AAA dismantleUIViewController")
-  }
 
   public func makeUIViewController(context _: Context) -> UINavigationController {
-    print("AAA makeUIViewController")
     let vc = UINavigationController()
     vc.setViewControllers(linkNavigator.launch(item: item), animated: false)
     return vc
   }
 
   public func updateUIViewController(_ uiViewController: UINavigationController, context _: Context) {
-    print("AAA updateUIViewController")
     linkNavigator.rootController = uiViewController
   }
 
