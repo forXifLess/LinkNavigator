2d495ddfd323ea74dc17bbb7f767b5ba8847b977
Scott Moon <interactord@gmail.com>
2024-01-03 10:36:00 +0900

    feat: add Base of TabExample

---
+++ Examples/TabNavigator/TabBasic/TabBasic/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
