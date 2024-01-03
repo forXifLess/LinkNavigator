eae5ea1257a366a811f92bf25767a79fad0fd02f
kyungseok.lee <lks574@naver.com>
2024-01-03 18:17:57 +0900

    feat: SingleNavigator - Implementing transitions between screens

--- Examples/SingleNavigator/01-SingleBasic/SingleBasic/Page/Page1/Page1View.swift
+++ Examples/SingleNavigator/01-SingleBasic/SingleBasic/Page/Page1/Page1View.swift
@@ -6,7 +6,49 @@ struct Page1View: View {
   let navigator: RootNavigatorType
 
   var body: some View {
-    Text("Page1View")
+    ScrollView {
+      VStack(spacing: 40) {
+        Text("1")
+          .font(.system(size: 70, weight: .thin))
+
+        Text(navigator.getCurrentPaths().map { $0.replacingOccurrences(of: "page", with: "") }.joined(separator: " → "))
+          .padding()
+
+        Button(action: {
+          navigator.next(linkItem: .init(path: "page2"), isAnimated: true)
+        }) {
+          Text("go to next Page")
+        }
+
+        Button(action: {
+          navigator.backOrNext(linkItem: .init(path: Bool.random() ? "home" : "page2"), isAnimated: true)
+        }) {
+          Text("backOrNext")
+        }
+
+        Button(action: {
+          navigator.rootBackOrNext(linkItem: .init(path: Bool.random() ? "home" : "page2"), isAnimated: true)
+        }) {
+          Text("**root** backOrNext")
+        }
+
+        Button(action: {
+          navigator.back(isAnimated: true)
+        }) {
+          Text("back")
+        }
+
+        Button(action: {
+          navigator.close(isAnimated: true, completeAction: { })
+        }) {
+          Text("close (only available in modal)")
+            .foregroundColor(.red)
+        }
+
+        Spacer()
+      }
+      .padding()
+    }
   }
 }
 
--- Examples/SingleNavigator/01-SingleBasic/SingleBasic/Page/Page4/Page4View.swift
+++ Examples/SingleNavigator/01-SingleBasic/SingleBasic/Page/Page4/Page4View.swift
@@ -6,6 +6,36 @@ struct Page4View: View {
   let navigator: RootNavigatorType
 
   var body: some View {
-    Text("Page4View")
+    ScrollView {
+      VStack(spacing: 40) {
+        Text("4")
+          .font(.system(size: 70, weight: .thin))
+
+        Text(navigator.getCurrentPaths().map { $0.replacingOccurrences(of: "page", with: "") }.joined(separator: " → "))
+          .padding()
+
+        Button(action: {
+          navigator.backOrNext(linkItem: .init(path: "home"), isAnimated: true)
+        }) {
+          Text("back to Home")
+        }
+
+        Button(action: {
+          navigator.back(isAnimated: true)
+        }) {
+          Text("back")
+        }
+
+        Button(action: {
+          navigator.replace(linkItem: .init(path: "home"), isAnimated: false)
+        }) {
+          Text("reset")
+            .foregroundColor(.red)
+        }
+
+        Spacer()
+      }
+      .padding()
+    }
   }
 }
