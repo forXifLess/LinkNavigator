74e3563d40371873ee662ef9647c775a99af611a
Scott Moon <interactord@gmail.com>
2024-01-03 09:31:57 +0900

    feat: New Example Project

---
+++ Examples/SingleNavigator/SingleBasic/SingleBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SingleBasic
+//
+//  Created by SANGBONG MOON on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
