e769d70a0d84c51637ce854490a6223a7446b772
Scott Moon <interactord@gmail.com>
2024-01-03 11:46:00 +0900

    feat: added base of Example

---
+++ Examples/SingleNavigator/SingleEventSubscriber/SingleEventSubscriber/SingleEventSubscriberApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SingleEventSubscriberApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Examples/TabNavigator/TabEventSubscriber/TabEventSubscriber/TabEventSubscriberApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct TabEventSubscriberApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
