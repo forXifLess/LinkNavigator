093c43825c3d198d45a2a6d2c4f2300121d0a436
kyungseok.lee <lks574@naver.com>
2024-01-04 10:23:26 +0900

    feat: SingleNavigator02 - Initial Page Setup

---
+++ Examples/SingleNavigator/02-SingleEventSubscriber/SingleEventSubscriber/Page/Page1/Page1RouteBuilder.swift
@@ -0,0 +1,14 @@
+import LinkNavigator
+import SwiftUI
+
+struct Page1RouteBuilder<RootNavigator: RootNavigatorType> {
+
+  static func generate() -> RouteBuilderOf<RootNavigator> {
+    var matchPath: String { "page1" }
+    return .init(matchPath: matchPath) { navigator, items, diContainer -> RouteViewController? in
+      return WrappingController(matchPath: matchPath) {
+        Page1View(navigator: navigator)
+      }
+    }
+  }
+}
