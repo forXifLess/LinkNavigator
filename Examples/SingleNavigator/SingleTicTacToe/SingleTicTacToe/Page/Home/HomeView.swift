b91a00ad37de89103ae7388be171d020bd644a72
kyungseok.lee <lks574@naver.com>
2024-01-08 19:01:24 +0900

    feat: Temp

--- Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/Home/HomeView.swift
+++ Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/Home/HomeView.swift
@@ -5,7 +5,26 @@ struct HomeView: View {
 
   let navigator: RootNavigatorType
 
+  @State private var isLogin = UserDefaults.standard.bool(forKey: "isLogin")
+
   var body: some View {
-    Text("HomeView")
+    VStack(spacing: 30) {
+      PathIndicator(currentPath: navigator.getCurrentPaths().joined(separator: " -> "))
+        .padding(.top, 32)
+      
+      Button(action: {
+        navigator.sheet(linkItem: .init(path: "login"), isAnimated: true)
+      }) {
+        Text("go to Login")
+      }
+
+      Spacer()
+    }
+    .padding()
+    .onAppear {
+      if isLogin {
+        navigator.replace(linkItem: .init(path: "newGame"), isAnimated: true)
+      }
+    }
   }
 }
--- Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/NewGame/NewGameView.swift
+++ Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/NewGame/NewGameView.swift
@@ -6,6 +6,19 @@ struct NewGameView: View {
   let navigator: RootNavigatorType
 
   var body: some View {
-    Text("NewGameView")
+    VStack(spacing: 30) {
+      PathIndicator(currentPath: navigator.getCurrentPaths().joined(separator: " -> "))
+        .padding(.top, 32)
+
+      Button(action: {
+        UserDefaults.standard.setValue(false, forKey: "isLogin")
+        navigator.replace(linkItem: .init(path: "home"), isAnimated: true)
+      }) {
+        Text("logout")
+      }
+
+      Spacer()
+    }
+    .padding()
   }
 }
