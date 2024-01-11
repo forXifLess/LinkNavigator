34b1fa482c9f0044bf170f8629e0743c876624a9
kyungseok.lee <lks574@naver.com>
2024-01-11 16:47:41 +0900

    feat: SingleNavigator03 - Routing and Login

--- Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/Login/LoginView.swift
+++ Examples/SingleNavigator/SingleTicTacToe/SingleTicTacToe/Page/Login/LoginView.swift
@@ -5,7 +5,34 @@ struct LoginView: View {
 
   let navigator: RootNavigatorType
 
+  @State private var message: String = ""
+  @State private var password: String = ""
+
   var body: some View {
-    Text("LoginView")
+    VStack(spacing: 30) {
+      PathIndicator(currentPath: navigator.getCurrentPaths().joined(separator: " -> "))
+        .padding(.top, 32)
+
+      VStack(spacing: 16) {
+        TextField("id", text: $message)
+          .textFieldStyle(.roundedBorder)
+          .padding(.horizontal)
+
+        SecureField("password", text: $password)
+          .textFieldStyle(.roundedBorder)
+          .padding(.horizontal)
+
+        Button(action: {
+          UserDefaults.standard.setValue(true, forKey: "isLogin")
+          navigator.close(isAnimated: true) {
+            navigator.rootReloadLast(items: .init(path: "home"), isAnimated: true)
+          }
+        }) {
+          Text("Login")
+        }
+      }
+      Spacer()
+    }
+    .padding()
   }
 }
